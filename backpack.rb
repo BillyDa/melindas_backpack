585326c897e057db932b6c4f3c1a26a756a852d6
Billy Davidson <billy.davids52@gmail.com>
2018-08-29 14:40:43 -0400

    code smells 5 and 6, deleted dead code and unnecessary comments

--- backpack.rb
+++ backpack.rb
@@ -20,9 +20,9 @@ class Backpack
 
   end
 
+  # Ensure gym shoes are added to backpack if it's a gym day
   def shoes(day_of_week)
     if day_of_week == 'monday' || day_of_week == 'thursday'
-      #add gym shoes to items
       @items << 'gym shoes'
     end
   end
@@ -30,8 +30,7 @@ class Backpack
   def meals(day_of_week)
     if day_of_week != 'saturday' && day_of_week != 'sunday'
       @items << 'packed lunch'
-    elsif false
-      # Bring snacks on weekend trips
+    else
       @items << 'snacks'
     end
   end
@@ -41,21 +40,12 @@ class Backpack
     # set up local variables used by rest of prepare method
     conditions = @attributes[:weather]
     day_of_week = @attributes[:day_of_week]
-
-    # Ensure appropriate clothing is added to backpack
-
+    
     outfit(conditions)
     shoes(day_of_week)
     meals(day_of_week)
   end
-    # Ensure gym shoes are added to backpack if it's a gym day
-    # Gotta get to the gym on Mondays and Thursdays.
 
-
-    # Bring a packed lunch on all weekdays
-
-
-  # Prints a summary packing list for Melinda's backpack
   def packing_list
     output = []
     output << "Melinda, here's your packing list!"
